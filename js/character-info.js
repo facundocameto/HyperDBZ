4ee2a3e0bf2a6f6fa710be5df7fce83596496e04
facundocameto <68877564+facundocameto@users.noreply.github.com>
2020-12-19 16:06:14 -0300

    upd

--- js/character-info.js
+++ js/character-info.js
@@ -1,4 +1,65 @@
 
+function createCarousel(characterArray){
+
+//get character clicked from URL
+  var params = new URLSearchParams(location.search);
+  var nameURL = params.get('name');
+
+  //check what character is
+    let htmlContentToAppend = "";
+    for(let i = 0; i < characterArray.length; i++){
+        let character = characterArray[i];
+
+        if (nameURL == character.name)
+        {
+          createCarousel(character.ingImg);
+          function createCarousel(array)
+          {
+            let htmlCont = "";
+            htmlCont = `
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>`
+                for(let i = 1; i < array.length; i++){
+                    let image = array[i];
+
+                    htmlCont += `
+                  <li data-target="#carouselExampleIndicators" data-slide-to="` + i + `"></li>
+                  `
+                }
+              htmlCont += `
+              </ol>
+              <div style="max-width: 1000px; min-width: 320px; margin-left:auto; margin-right:auto; background-color: black; border-top-right-radius: 20%; border-bottom-left-radius: 20%;" class="carousel-inner">
+              <div style="" class="carousel-item hdbz-trailer active">
+                <img style="width: 100%; height: 100%; object-fit: cover; object-position: center;" class="d-block img-fluid" src="` + array[0] + `" alt="First slide">
+                <div class="border-trailer"></div>
+              </div>
+              `
+              for(let i = 1; i < array.length; i++){
+                  let image = array[i];
+
+                  htmlCont += `
+                <div style="" class="carousel-item hdbz-trailer">
+                  <img style="width: 100%; height: 100%; object-fit: cover; object-position: center;" class="d-block img-fluid" src="` + image + `" alt="First slide">
+                  <div class="border-trailer"></div>
+                </div>
+                `
+              }
+              htmlCont += `<a style="opacity: 100%;" class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a style="opacity: 100%;" class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a></div>
+            </div>`
+
+            document.getElementById("carouselExampleIndicators").innerHTML = htmlCont
+          }
+        }
+      }
+    }
+
 function showCharacterInfo(characterArray){
 
 //get character clicked from URL
@@ -16,41 +77,10 @@ function showCharacterInfo(characterArray){
             htmlContentToAppend += `
             <div class="row text-center ml-3 mr-3">
               <div class="col-sm mb-3 p-0 comunity-title"> <h1 class="p-2">` + character.name + `</h1> </div>
-            </div>` + createCarousel(character.ingImg) + `
-            <div style="margin-top: -2rem;" class="text-center ml-3 mr-3 char-sent"><img src="` + character.image + `"><p style="margin-top: -2rem;" class="character-text">` + `"` + character.sentence + `"` + `</p></div>
+            </div><div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel"></div>
+            <div class="text-center ml-3 mr-3 char-sent"><p class="character-text">` + `"` + character.sentence + `"` + `</p></div>
             <div class="col-sm text-center char-desc character-text"><p>` + character.description + `</p></div>
             `
-            function createCarousel(array)
-            {
-              htmlContentToAppend += `
-              <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                ` +
-                for(let i = 0; i < array.length; i++){
-                    let image = array[i];
-
-                    htmlContentToAppend += `
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src="` + image + `" alt="First slide">
-                  </div>
-                  `
-                } +
-                `</div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </div>`
-            }
             function showGif(array)
             {
               htmlContentToAppend += `<div class="row justify-content-center text-center w-100 mt-3 ml-0 mr-0">`
@@ -117,6 +147,7 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CHARACTERS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             showCharacterInfo(resultObj.data);
+            createCarousel(resultObj.data);
         }
     });
 });
